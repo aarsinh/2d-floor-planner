f322b560d6ceb7d289bf2f12d533dd223fd76c93
aarsinh <148481261+aarsinh@users.noreply.github.com>
2024-09-18 22:40:32 +0530

    refactor

--- src/main/java/com/floorplanner/FloorPlanner.java
+++ src/main/java/com/floorplanner/FloorPlanner.java
@@ -1,6 +1,7 @@
+package src.main.java.com.floorplanner;
 import javax.swing.*;
 
-public class MainWindow extends JFrame {
+public class FloorPlanner extends JFrame {
     public static void main(String[] args) {
         JFrame frame = new JFrame("2D Floor Planner");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
