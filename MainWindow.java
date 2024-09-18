c9fb953d830c85a6a7163e72759554e00aefc32a
aarsinh <148481261+aarsinh@users.noreply.github.com>
2024-09-18 18:12:26 +0530

    initial commit

---
+++ MainWindow.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("2D Floor Planner");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1920, 1080);
+        frame.setVisible(true);
+    }
+}
