7d7ea30b5973158bd49c1caebde7767237b6cd45
PMuku <pmuku2812@gmail.com>
2024-10-20 16:47:42 +0530

    Modified Control Panel layout and added Save Panel

--- src/main/java/com/floorplanner/Canvas.java
+++ src/main/java/com/floorplanner/Canvas.java
@@ -1,17 +1,23 @@
 package src.main.java.com.floorplanner;
+import java.awt.BorderLayout;
+
 import javax.swing.*;
 
 class Canvas extends JFrame {
     private ControlPanel controlPanel;
-    
-    public void FloorPlanner() {
-        this.controlPanel = new ControlPanel();
-        JFrame frame = new JFrame("2D Floor Planner");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(null);
-        frame.add(controlPanel);
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+    private SavePanel savePanel;
 
-        frame.setVisible(true);
+    public void FloorPlanner() {
+        setTitle("2D Floor Planer");
+        this.controlPanel = new ControlPanel();
+        this.savePanel = new SavePanel();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        add(controlPanel, BorderLayout.WEST);
+        add(savePanel, BorderLayout.EAST);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setVisible(true);
+
+        this.savePanel = new SavePanel();
     }
 }
