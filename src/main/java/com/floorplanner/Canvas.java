a632662350bb01db1318b11f734b4ea9aaf14555
aarsinh <148481261+aarsinh@users.noreply.github.com>
2024-11-09 20:13:57 +0530

    fixed overlap when creating rooms

--- src/main/java/com/floorplanner/Canvas.java
+++ src/main/java/com/floorplanner/Canvas.java
@@ -1,30 +1,25 @@
 package src.main.java.com.floorplanner;
 import java.awt.*;
-
 import javax.swing.*;
+
 
 class Canvas extends JFrame {
     private ControlPanel controlPanel;
     private SavePanel savePanel;
-    private MiddlePanel middlePanel;
-
-    public Canvas() {  
+    public Canvas() {
         setTitle("2D Floor Planer");
         this.controlPanel = new ControlPanel();
         this.savePanel = new SavePanel();
-        this.middlePanel = new MiddlePanel();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(null);
-        controlPanel.setBounds(0, 0, 350, 1000);
-        savePanel.setBounds(1720, 200, 200, 600);
-        middlePanel.setBounds(450, 80, 1200, 840);
-        
-        add(controlPanel);
-        add(middlePanel);
-        add(savePanel);
-        
+        setLayout(new BorderLayout());
+        add(controlPanel, BorderLayout.WEST);
+        add(controlPanel.mPanel, BorderLayout.CENTER);
+        add(savePanel, BorderLayout.EAST);
+
+        //m.setLayout(null);
+
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setVisible(true);
-        System.out.println(Toolkit.getDefaultToolkit().getScreenSize());
+        //this.savePanel = new SavePanel();
     }
 }
