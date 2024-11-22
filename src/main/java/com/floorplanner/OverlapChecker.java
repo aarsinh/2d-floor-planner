af3b43c8333f643952aae6b3bc2b413753cd510f
aarsinh <148481261+aarsinh@users.noreply.github.com>
2024-11-22 17:43:20 +0530

    Fixed overlapping of furniture/fixture with borders

--- src/main/java/com/floorplanner/OverlapChecker.java
+++ src/main/java/com/floorplanner/OverlapChecker.java
@@ -10,21 +10,24 @@ class OverlapChecker{
     public static boolean roomOverlap(CanvasElement newElement, int x, int y, String type) {
         Rectangle newBounds = new Rectangle(x, y, newElement.getWidth(), newElement.getHeight());
 
-        // Iterable<? extends CanvasElement>
-        if(type.equals("Room")) {
+        Iterable<? extends CanvasElement> elementsToCheck = type.equals("Room") ? CanvasElement.rooms : CanvasElement.elements;
+        for (CanvasElement e : elementsToCheck) {
+            if (e != newElement && e.getBounds().intersects(newBounds)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean borderOverlap(CanvasElement e, int x, int y) {
+        Rectangle newBounds = new Rectangle(x, y, e.getWidth(), e.getHeight());
+        if(!e.getType().equals("Room")) {
             for (Room r : CanvasElement.rooms) {
-                if (r != newElement && r.getBounds().intersects(newBounds)) {
+                if(r.getBounds().intersects(newBounds) && !r.getBounds().contains(newBounds)) {
                     return true;
                 }
             }
-        } else if(!type.equals("Room")){
-            Rectangle currentBounds = new Rectangle(x, y, ControlPanel.iconToBounds.get(type)[0], ControlPanel.iconToBounds.get(type)[1]);
-            for (CanvasElement e : CanvasElement.elements){
-                Rectangle eBounds = new Rectangle(e.getX(), e.getY(), ControlPanel.iconToBounds.get(e.getType())[0], ControlPanel.iconToBounds.get(e.getType())[1]);
-                if (e != newElement && eBounds.intersects(currentBounds)) {
-                    return true;
-                }
-            }
+            return false;
         }
         return false;
     }
